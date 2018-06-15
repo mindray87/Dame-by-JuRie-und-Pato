d9f2765f3b2bfafe26dd030b1e2ff33729b6a532
JulianRiegraf <julianriegraf@web.de>
2018-06-15 11:04:41 +0200

    implement controller init

--- de.HTWG.se.Dame/src/test/scala/controller/ControllerSpec.scala
+++ de.HTWG.se.Dame/src/test/scala/controller/ControllerSpec.scala
@@ -1,20 +1,16 @@
 package controller
 
 import de.HTWG.se.Dame.controller.Controller
-import de.HTWG.se.Dame.model.enums.Color
-import de.HTWG.se.Dame.model.{Cell, Grid, Piece, Player}
 import org.scalatest.{Matchers, WordSpec}
 
 class ControllerSpec extends WordSpec with Matchers{
   "A move is valid" when {
     "the destinationcell is empty" should {
-      val grid = new Grid(8)
-      val player = new Player("Hansi", grid, Color.White)
-      val controller = new Controller(grid)
+      val controller = new Controller("Julian", "Patrick", 8)
       "move" in {
-        controller.move(4, 4, player.pieces(0))
-        val opt = grid.getCell(4, 4)
-        opt.get.piece should be(Some(player.pieces(0)))
+        controller.move(4, 4, controller.p1.pieces(0))
+        val opt = controller.grid.getCell(4, 4)
+        opt.get.piece should be(Some(controller.p1.pieces(0)))
       }
     }
   }
